18e476633f3695637e6ecf47da78811ef7c9075b
Dirk Kuhn <die_pizza@web.de>
2023-11-28 17:20:45 +0100

    Before changing Factor Generator

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -37,15 +37,15 @@ class MonteCarloSimulation:
         self.factors_gen = _create_factors_gen(
             investment_return_gen, safe_deposit_rate_gen, inflation_rate_gen
         )
-        self.values: ti.Field
+        self.values = ti.field(dtype=float, shape=(self.num_sim, 2))
 
     def __call__(
             self, num_years: int,
             current_invest: float, current_save: float,
             monthly_invest: float, monthly_save: float
     ) -> SimulationResult:
-        self._create_fields(num_years)
         self._simulate(
+            num_months=num_years*12,
             current_invest=current_invest, current_save=current_save,
             monthly_invest=monthly_invest, monthly_save=monthly_save
         )
@@ -56,52 +56,23 @@ class MonteCarloSimulation:
             current_save=current_save,
             monthly_invest=monthly_invest,
             monthly_save=monthly_save,
-            value=values[:, :, 0],
-            value_only_safe_deposit=values[:, :, 1]
+            value=values[:, 0],
+            value_only_safe_deposit=values[:, 1]
         )
-
-    def _create_fields(self, num_years: int) -> None:
-        self.values = ti.field(dtype=float, shape=(self.num_sim, 12*num_years, 2))
 
     @ti.kernel
     def _simulate(
-            self,
+            self, num_months: int,
             current_invest: float, current_save: float,
             monthly_invest: float, monthly_save: float
     ):
         for i in range(self.num_sim):  # Parallel
-            acc_invest = acc_total_invested = current_invest + monthly_invest
-            acc_save = acc_total_saved = current_save + monthly_save
+            acc_invest = current_invest
+            acc_save = current_save
             acc_only_save = acc_invest + acc_save
             acc_inflation = 1.0
 
-            for j in range(self.num_months):  # Sequential
-                # Portfolio value before taxes and inflation
-                value = acc_invest + acc_save
-                value_only_safe = acc_only_save
-
-                # Portfolio value after taxes and before inflation
-                profit_invest = acc_invest - acc_total_invested
-                profit_save = acc_save - acc_total_saved
-                profit_only_save = value_only_safe - (acc_total_invested + acc_total_saved)
-
-                # Losses between ETFs and bonds/savings accounts are settled
-                tax_invest = max(0., profit_invest+min(0., profit_save)) \
-                    * (1.-self.investment_tax_exemption) * self.capital_gains_tax_rate
-                tax_safe = max(0., profit_save+min(0., profit_invest)) * self.capital_gains_tax_rate
-                tax_only_save = max(0., profit_only_save) * self.capital_gains_tax_rate
-
-                value -= tax_invest + tax_safe
-                value_only_safe -= tax_only_save
-
-                # Portfolio value after inflation
-                value /= acc_inflation
-                value_only_safe /= acc_inflation
-
-                # Store portfolio value
-                self.values[i, j, 0] = value
-                self.values[i, j, 1] = value_only_safe
-
+            for j in range(num_months):  # Sequential
                 # Update accumulators
                 invest_increase, safe_increase, inflation_increase = self.factors_gen.sample()
                 acc_invest = (acc_invest + monthly_invest) * invest_increase
@@ -109,9 +80,35 @@ class MonteCarloSimulation:
                 acc_only_save = (acc_only_save + monthly_invest + monthly_save) * safe_increase
                 acc_inflation *= inflation_increase
 
-    @property
-    def num_months(self) -> int:
-        return self.values.shape[1]
+            # Portfolio value before taxes and inflation
+            value = acc_invest + acc_save
+            value_only_safe = acc_only_save
+
+            # Amount paid
+            invest_payment = current_invest + num_months * monthly_invest
+            save_payment = current_save + num_months * monthly_save
+
+            # Portfolio value after taxes and before inflation
+            profit_invest = acc_invest - invest_payment
+            profit_save = acc_save - save_payment
+            profit_only_save = value_only_safe - (invest_payment + save_payment)
+
+            # Losses between ETFs and bonds/savings accounts are settled
+            tax_invest = max(0., profit_invest + min(0., profit_save)) \
+                * (1. - self.investment_tax_exemption) * self.capital_gains_tax_rate
+            tax_safe = max(0., profit_save + min(0., profit_invest)) * self.capital_gains_tax_rate
+            tax_only_save = max(0., profit_only_save) * self.capital_gains_tax_rate
+
+            value -= tax_invest + tax_safe
+            value_only_safe -= tax_only_save
+
+            # Portfolio value after inflation
+            value /= acc_inflation
+            value_only_safe /= acc_inflation
+
+            # Store portfolio value
+            self.values[i, 0] = value
+            self.values[i, 1] = value_only_safe
 
 
 def _create_factors_gen(
@@ -164,7 +161,7 @@ if __name__ == '__main__':
     )
 
     sim = MonteCarloSimulation(
-        num_sim=100_000,
+        num_sim=10_000,
         capital_gains_tax_rate=0.278186,
         investment_tax_exemption=0.3,
         investment_return_gen=HistoricalACWIIMIReturns(),
@@ -177,11 +174,8 @@ if __name__ == '__main__':
     for i in range(10):
         t = time()
         res = sim(
-            num_years=200,
+            num_years=500,
             current_invest=10_000, current_save=10_000,
             monthly_invest=1_000, monthly_save=1_000
         )
         print(f'Run {i}, duration: {time()-t}')
-        del res
-
-    pass
